e31bc25a72b41ecec99c20a2c04c3888362b2049
Tanner <Tanner.Hall@unb.ca>
2026-05-16 13:39:13 -0300

    added semi colon after defining value and let the while loop include 10 so it prints all of the proper numbrs.

--- Assignment_1/tablesqrcb.c
+++ Assignment_1/tablesqrcb.c
@@ -5,8 +5,8 @@ int main(void)
 printf("%s","Number \t");
 printf("%s","square \t");
 printf("%s","cube \n");
-int value =0
-while (value < 10)
+int value =0;
+while (value <= 10)
     {
 
     printf("%d\t", value);
