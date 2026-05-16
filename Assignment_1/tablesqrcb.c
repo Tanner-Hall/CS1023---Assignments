7e98d07d26d51f2e475b7a8900d608b29b7f2a3c
Tanner <Tanner.Hall@unb.ca>
2026-05-16 13:37:30 -0300

    changed the for loop to while loop ad defined value before while loop

--- Assignment_1/tablesqrcb.c
+++ Assignment_1/tablesqrcb.c
@@ -5,9 +5,10 @@ int main(void)
 printf("%s","Number \t");
 printf("%s","square \t");
 printf("%s","cube \n");
-for(int counter=0; counter <10; counter++)
+int value =0
+while (value < 10)
     {
-    int value =0;
+
     printf("%d\t", value);
     printf("%d\t", value*value);
     printf("%d\n", value*value*value);
