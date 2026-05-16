62a10b5760b796c5b5622890e4f192607a487d68
Tanner <Tanner.Hall@unb.ca>
2026-05-16 13:32:52 -0300

    first attempt

---
+++ Assignment_1/tablesqrcb.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) 
+{
+printf("%s","Number \t");
+printf("%s","square \t");
+printf("%s","cube \n");
+for(int counter=0; counter <10; counter++)
+    {
+    int value =0;
+    printf("%d\t", value);
+    printf("%d\t", value*value);
+    printf("%d\t", value*value*value);
+    }
+
+}
