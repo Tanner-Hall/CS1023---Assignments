eb79ce9c633c00fe3cb58ec0ad46c374aa1033e3
Tanner <Tanner.Hall@unb.ca>
2026-05-22 14:27:26 -0300

    case 2 working for hourly pay

--- Assignment_2/question2/empay.c
+++ Assignment_2/question2/empay.c
@@ -16,6 +16,18 @@ int main(void) {
                 pay = salary;
                 break;
             }
+             case 2: {
+                double wage, hours;
+                printf("Enter hourly wage: ");
+                scanf("%lf", &wage);
+                printf("Enter hours worked: ");
+                scanf("%lf", &hours);
+                if (hours <= 40)
+                    pay = wage * hours;
+                else
+                    pay = wage * 40 + wage * 1.5 * (hours - 40);
+                break;
+            }
             default:
                 printf("Invalid pay code. Please enter 1-4.\n");
                 break;
