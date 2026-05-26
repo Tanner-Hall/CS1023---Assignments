4e404b413b12902931347b4d932cf7fc57dde3c2
Tanner <Tanner.Hall@unb.ca>
2026-05-26 15:54:26 -0300

    main function

---
+++ Assignment_3/question1/divrecur.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int gcd(int x,int y);
+
+int main(void){
+int a,b;
+puts("enter the two numbers you wish to find the greatest common divisor of");
+scanf("%d", &a);
+scanf("%d", &b);
+printf("the greatest common divisor of %d and %d is %d\n",a,b, gcd(a,b));
+
+}
