060c5b29a037bf686bb0c38fc9b31669f4ddb7b0
Tanner <Tanner.Hall@unb.ca>
2026-06-12 15:22:56 -0300

    added a way to validate that string is only R, L, _

--- Assignment_4/Question_1/Furthest.c
+++ Assignment_4/Question_1/Furthest.c
@@ -3,11 +3,26 @@
 int furthestDistanceFromOrigin(const char moves[]);
 
 int main() {
-    char userInput[256]; 
+char userInput[256]; 
+int isValid = 1; 
 
-    printf("Enter a movement string (L, R, _): ");
-    scanf("%255s", userInput); 
+printf("Enter a movement string (L, R, _): ");
+scanf("%255s", userInput); 
 
-        int result = furthestDistanceFromOrigin(userInput);
-        printf("The furthest distance possible is: %d\n", result);
+int i = 0;
+while (userInput[i] != '\0') {
+    char current = userInput[i];
+        
+    if (current != 'L' && current != 'R' && current != '_') {
+        isValid = 0; 
+        break;     
+    }
+    i++; 
+}
+if (isValid == 0) {
+    printf("Error: Invalid characters detected. Use only L, R, or _.\n");
+} else {
+    int result = furthestDistanceFromOrigin(userInput);
+    printf("The furthest distance possible is: %d\n", result);
+}
 }
