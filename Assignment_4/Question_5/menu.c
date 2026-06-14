7d5a318f82d597c552f92b3f23801b7536810927
Tanner <Tanner.Hall@unb.ca>
2026-06-14 18:49:01 -0300

    addition function done

--- Assignment_4/Question_5/menu.c
+++ Assignment_4/Question_5/menu.c
@@ -43,3 +43,8 @@ printf("\n");
 printf("\n");
 }
 }
+void add(double a, double b) {
+printf("[CALCULATION TYPE]: Addition\n");
+printf("[INPUT PARAMETERS]: Value A = %.4f | Value B = %.4f\n", a, b);
+printf("[EXECUTION RESULT]: %.4f + %.4f = %.4f\n", a, b, a + b);
+}
