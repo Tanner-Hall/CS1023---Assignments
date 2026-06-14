989463d9b8d66c83907638f18aa644b4b34b8cdd
Tanner <Tanner.Hall@unb.ca>
2026-06-14 17:48:10 -0300

    created a recursive function that will check the surroundings of it at a point and call on itself based on that info

--- Assignment_4/Question_3/recmaz.c
+++ Assignment_4/Question_3/recmaz.c
@@ -39,3 +39,29 @@ int main() {
     printf("\n FINAL MAZE PATH \n");
     printMaze(maze);
 }
+int solveMaze(char maze[ROWS][COLS], int r, int c) {
+    if (r < 0 || r >= ROWS || c < 0 || c >= COLS) {
+        return 0;
+    }
+    if (maze[r][c] != '.') {
+        return 0;
+    }
+
+    maze[r][c] = 'x';
+    printf("Moving to [%d, %d]:\n", r, c);
+    printMaze(maze);
+    printf("\n");
+    if (c == COLS - 1) {
+        return 1; 
+    }
+    if (solveMaze(maze, r, c + 1)) return 1;
+    if (solveMaze(maze, r + 1, c)) return 1;
+    if (solveMaze(maze, r, c - 1)) return 1;
+    if (solveMaze(maze, r - 1, c)) return 1;
+    maze[r][c] = ' '; 
+    printf("Dead end hit at [%d, %d]! Backtracking...\n", r, c);
+    printMaze(maze);
+    printf("\n");
+    
+    return 0;
+}
